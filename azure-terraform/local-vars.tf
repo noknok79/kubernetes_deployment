27d2410afcf7f0d58a010e1b9274ccec1976c80a
root <root@LAPTOP-OJCSKH2M>
2023-12-29 22:22:19 +0800

    include terraform.tfvars

--- azure-terraform/local-vars.tf
+++ azure-terraform/local-vars.tf
@@ -11,9 +11,9 @@ locals {
 #When the keys are numeric, it must use ":" instead of "="
 locals {
   web_inbound_port_map = {
-    "100" : "80",
-    "200" : "443",
-    "300" : "22"
+    "140" : "80",
+    "240" : "443",
+    "340" : "22"
   }
 }
 
@@ -21,22 +21,22 @@ locals {
   app_inboud_port_map = {
     "110" : "80",
     "210" : "443",
-    #"310" : "8080"
-    #"410" : "22"
+    "410" : "8080"
+    #"360" : "22"
   }
 }
 
 locals {
   db_inboud_port_map = {
     "120" : "3306",
-    #"220" : "1433"
+    "220" : "1433"
     "320" : "5432"
   }
 }
 
 locals {
   bastion_inboud_port_map = {
-    "130" : "22"
+    #"350" : "22"
     "230" : "3389"
   }
 }
