e86d8f5911ab34ae946d6a06767d3128b8c6479f
noknok79 <nokiev@yahoo.com>
2024-01-10 05:33:29 +0800

    terraform deployment in azure devops

--- sample_deployment/main.tf
+++ sample_deployment/main.tf
@@ -29,9 +29,10 @@ terraform {
   # Terraform State Storage to Azure Storage Container
   backend "azurerm" {
     resource_group_name  = "terraform-aks-storage-rg"
-    storage_account_name = "terratfstatestrg"
-    container_name       = "tfstatefiles"
-    key                  = "uat.terraform.tfstate"
+    storage_account_name = "terratfstatestrg1"
+    #storage_account_name = "terratfstatestrg"
+    container_name = "tfstatefiles"
+    key            = "uat.terraform.tfstate"
   }
 }
 
